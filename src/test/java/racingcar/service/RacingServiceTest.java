db90be924ec17aad2ca2e9390292d9ee37527a5f
SooHyeon Lee <dalsu4594@naver.com>
2025-10-27 17:55:29 +0900

    test: 서비스 로직 변경으로 인한 수정
    - 서비스 로직 통합으로 인한 테스트 문제 발생
    - runGame() 메서드로 테스트 로직 변경

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -5,41 +5,61 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
-import racingcar.dto.CarStatusDto;
+import racingcar.dto.GameResultDto;
+import racingcar.dto.RoundResultDto;
 import racingcar.exception.DomainValidationException;
 import racingcar.exception.InputParsingException;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static racingcar.exception.message.ErrorMessage.DUPLICATE_CAR_NAME;
-import static racingcar.exception.message.ErrorMessage.EMPTY_CAR_NAMES;
+import static racingcar.exception.message.ErrorMessage.*;
 
 public class RacingServiceTest {
     private RacingService racingService;
+    private String tryCount;
 
     @BeforeEach
-    void set(){
+    void set() {
         racingService = new RacingService(() -> 4);
+        tryCount = "5";
     }
 
     @Test
-    @DisplayName("자동차 이름을 입력받아 Cars 객체를 생성한다.")
-    void createCars(){
-        Cars cars = racingService.createCars("pobi,crong,ady");
-        assertThat(cars.getCars())
-                .extracting(Car::getNameValue)
-                .containsExactly("pobi", "crong", "ady");
+    @DisplayName("게임 실행 후 라운드 결과와 우승자를 반환한다.")
+    void runGameReturnGameResult() {
+        GameResultDto gameResults = racingService.runGame("pobi, crong, ady", tryCount);
+        assertThat(gameResults.getRoundResults()).hasSize(5);
+        assertThat(gameResults.getWinnerCarNames()).containsExactly("pobi", "crong", "ady");
+    }
+
+    @Test
+    @DisplayName("랜덤 값이 4 이상이면 자동차가 이동한다")
+    void moveWhenRandomNumberIsGreaterThanOrEqualToFour() {
+        GameResultDto gameResult = racingService.runGame("pobi,crong,ady", tryCount);
+        List<RoundResultDto> roundResults = gameResult.getRoundResults();
+        RoundResultDto roundResult = roundResults.get(4);
+        assertThat(roundResult.getCarStatuses()).allMatch(status -> status.position() == 5);
+    }
+
+    @Test
+    @DisplayName("랜덤 값이 3 이하면 자동차가 이동하지 않는다.")
+    void notMoveWhenRandomNumberIsLessThanFour(){
+        racingService = new RacingService(() -> 3);
+        GameResultDto gameResult = racingService.runGame("pobi,crong,ady", tryCount);
+        List<RoundResultDto> roundResults = gameResult.getRoundResults();
+        for (RoundResultDto roundResult : roundResults) {
+            assertThat(roundResult.getCarStatuses())
+                    .allMatch(status -> status.position() == 0);
+        }
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "", ",,",", ,"," , "
+            "", ",,", ", ,", " , "
     })
     @DisplayName("파싱 결과가 비어있다면 예외가 발생한다.")
-    void throwExceptionWhenInvalidInput(String input){
-        assertThatThrownBy(() -> racingService.createCars(input))
+    void throwExceptionWhenInvalidInput(String input) {
+        assertThatThrownBy(() -> racingService.runGame(input, tryCount))
                 .isInstanceOf(InputParsingException.class)
                 .hasMessage(EMPTY_CAR_NAMES.getMessage());
     }
@@ -47,36 +67,36 @@ public class RacingServiceTest {
     @Test
     @DisplayName("중복된 자동차 이름으로 생성 시 예외가 발생한다.")
     void throwExceptionWhenDuplicateCarNames(){
-        assertThatThrownBy(() -> racingService.createCars("pobi,crong,pobi"))
+        assertThatThrownBy(() -> racingService.runGame("pobi,crong,pobi", tryCount))
                 .isInstanceOf(DomainValidationException.class)
                 .hasMessage(DUPLICATE_CAR_NAME.getMessage());
     }
 
-    @Test
-    @DisplayName("한 라운드를 진행하면 모든 자동차가 랜덤하게 이동한다")
-    void playRound() {
-        Cars cars = racingService.createCars("pobi,crong,ady");
-        racingService.playRound(cars);
-        List<CarStatusDto> statuses = racingService.getCarStatuses(cars);
-        assertThat(statuses).allMatch(status -> status.position() == 1);
+    @ParameterizedTest
+    @ValueSource(strings = {"ab","!@","3.5"})
+    @DisplayName("시도할 횟수가 유효하지 않은 처리를 받으면 예외처리한다.")
+    void throwExceptionWhenInputIsNotInteger(String count){
+        assertThatThrownBy(() -> racingService.runGame("pobi,crong,qq", count))
+                .isInstanceOf(InputParsingException.class)
+                .hasMessage(INVALID_NUMBER_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-5","0"})
+    @DisplayName("1보다 작은 수를 받으면 예외처리한다.")
+    void throwExceptionWhenInputIsLessThanOne(String count){
+        assertThatThrownBy(() -> racingService.runGame("pobi,crong,qq", count))
+                .isInstanceOf(InputParsingException.class)
+                .hasMessage(INVALID_TRY_COUNT.getMessage());
     }
 
     @Test
-    @DisplayName("랜덤 값이 3 이하면 자동차가 이동하지 않는다.")
-    void notMoveWhenRandomNumberIsLessThanFour(){
-        racingService = new RacingService(() -> 3);
-        Cars cars = racingService.createCars("pobi,crong,ady");
-        racingService.playRound(cars);
-        List<CarStatusDto> statuses = racingService.getCarStatuses(cars);
-        assertThat(statuses).allMatch(status -> status.position() == 0);
-    }
-
-    @Test
-    @DisplayName("우승자가 여러 명인 경우 모든 우승자 이름을 반환한다")
-    void findMultipleWinners() {
-        Cars cars = racingService.createCars("pobi,crong,ady");
-        racingService.playRound(cars);
-        List<String> winners = racingService.findWinners(cars);
-        assertThat(winners).containsExactly("pobi", "crong", "ady");
+    @DisplayName("빈 입력을 받는 경우 예외처리한다.")
+    void throwExceptionWhenInputIsEmpty(){
+        String count = " ";
+        assertThatThrownBy(() -> racingService.runGame("pobi,crong,qq", count))
+                .isInstanceOf(InputParsingException.class)
+                .hasMessage(EMPTY_TRY_COUNT.getMessage());
     }
 }
+
